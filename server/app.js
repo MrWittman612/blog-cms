fd4dfe470a1015021560b8b63854d2d3ef99a61e
Chad <40812274+MrWittman612@users.noreply.github.com>
2021-02-24 20:38:00 -0600

    add build script server code to server code to sever react build code

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const userRouter = require('./api/user/user.router');
 const blogRouter = require('./api/blog/blog.router');
 const newsRouter = require('./api/news/news.router');
 const adminRouter = require('./api/admin/admin.router');
+const path = require('path');
 const port = process.env.PORT || 3001;
 
 const jsonParser = express.json();
@@ -27,11 +28,22 @@ app.use('/api/user', userRouter);
 
 app.use('/api/blogs', blogRouter);
 
-
 app.use('/api/news', newsRouter);
 
 admin.use('/admin/api', adminRouter);
 // app.use('/api/admin', admin);
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    const staticFiles = express.static(path.join(__dirname, '../client/build'));
+    app.use(staticFiles);
+
+    app.get('*', function (_req, res) {
+        return res.sendFile(
+            path.join(__dirname, '../client/build', 'index.html')
+        );
+    });
+}
 
 function start() {
     app.listen(port, () => console.log('server started'));
