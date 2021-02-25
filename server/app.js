e486a4aefbdccb1d65357543937c7bd1253da3f6
Chad <40812274+MrWittman612@users.noreply.github.com>
2021-02-24 22:52:45 -0600

    change admin base api

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use('/api/blogs', blogRouter);
 app.use('/api/news', newsRouter);
 
 admin.use('/admin/api', adminRouter);
-// app.use('/api/admin', admin);
+app.use('/admin/api', admin);
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === 'production') {
 
 function start() {
     app.listen(port, () => console.log('server started'));
-    admin.listen(5000, console.log('admin started'));
+    // admin.listen(5000, console.log('admin started'));
 }
 
 exports.start = start;
